a92cb8b31816e1073a1f3717da30e07117c5b6a6
igor <cool.rainbow2012@yandex.ru>
2018-11-21 23:43:19 +0300

    Подчистил DetailsActivity

--- app/src/main/java/root/iv/androidacademy/activity/NewsDetailsActivity.java
+++ app/src/main/java/root/iv/androidacademy/activity/NewsDetailsActivity.java
@@ -1,14 +1,10 @@
 package root.iv.androidacademy.activity;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -35,21 +31,5 @@ public class NewsDetailsActivity extends AppCompatActivity{
         NewsItem newsItem = getIntent().getParcelableExtra(NewsItem.INTENT_TAG);
         setTitle(newsItem.getSubSection());
         webView.loadUrl(newsItem.getFullText());
-//        webView.setWebViewClient(new WebClient());
-    }
-
-    class  WebClient extends WebViewClient {
-        @TargetApi(Build.VERSION_CODES.N)
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-            view.loadUrl(request.getUrl().toString());
-            return true;
-        }
-
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
     }
 }
