77cd105bd2c1a4d1c5b52e5fa7950c24a6452a38
igor <cool.rainbow2012@yandex.ru>
2018-11-24 22:27:18 +0300

    Добавил иконку

---
+++ app/src/main/java/root/iv/androidacademy/activity/IntoActivity.java
@@ -0,0 +1,49 @@
+package root.iv.androidacademy.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Completable;
+import io.reactivex.disposables.Disposable;
+import root.iv.androidacademy.R;
+
+public class IntoActivity extends AppCompatActivity {
+    private static final String KEY_INTO = "KEY_INTO";
+
+    private Disposable disposable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        boolean into = getPreferences(MODE_PRIVATE).getBoolean(KEY_INTO, true);
+
+        if (into) {
+            setContentView(R.layout.activity_into);
+
+            disposable = Completable.complete()
+                    .delay(3, TimeUnit.SECONDS)
+                    .subscribe(this::startNewsListActivity);
+        } else {
+            startNewsListActivity();
+        }
+        getPreferences(MODE_PRIVATE).edit()
+                .putBoolean(KEY_INTO, !into)
+                .commit();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        disposable.dispose();
+    }
+
+    private void startNewsListActivity() {
+        Intent intent = new Intent(this, NewsListActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
